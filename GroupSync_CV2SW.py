9c9b1445480d73caaefe1def88cf414d58a9d56f
Nir <nir-r@bynet.co.il>
2023-05-04 17:23:40 +0300

    Creating External json file for CV assets

--- GroupSync_CV2SW.py
+++ GroupSync_CV2SW.py
@@ -21,12 +21,14 @@ import pandas as pd
 import logging
 import json
 import datetime
+import pytz
 from crayons import blue, red, green
 from dotenv import dotenv_values
 logging.basicConfig(filename="log.log", level=logging.INFO)
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 # This is the key to decrypt password 
+# tz = pytz.timezone('Asia/Jerusalem')
 key = b'oen-jEUBkzcMd7N8fg7z1vTB6iB36qIpaZMqDYjzbe4='
 fernet = Fernet(key)
 
@@ -71,7 +73,7 @@ headers = {
     'X-XSRF-TOKEN': cookies_dict["XSRF-TOKEN"]
 }
 
-
+asset_list=[]
 def is_valid_ipv4_address(address):
     try:
         ipaddress.IPv4Address(address)
@@ -91,15 +93,34 @@ def get_components_ip():
 
         # raw JSON data response
         raw_json_data = r_get.json()
-
+        # with open('data.json', 'w') as file:
+        #     json.dump(raw_json_data, file)
         # get only the label name and IP address of the component
         components_with_ip_and_label = {}
         for component in raw_json_data:
             output = component.get("group")
             if output is not None:
                 ip = component.get("ip")
+                mac = component.get("mac")
+                lastActivity = component.get("lastActivity")
+                tz = datetime.timezone(datetime.timedelta(hours=3), 'Israel')
+                local_time = datetime.datetime.fromtimestamp(lastActivity/1000, tz)
+                local_time_short = local_time.strftime('%Y-%m-%d %H:%M:%S')
+                id = component.get("id")  
+                group= component.get("group")
+                label = output.get("label")
+
                 if is_valid_ipv4_address(ip):
                     components_with_ip_and_label[ip] = output.get("label")
+                    my_asset_dict = {'ip_addr': ip, 'mac_addr': mac, 'last_activity':local_time_short,'cv_id':id, 'cv_label':label}
+                    asset_list.append(my_asset_dict)
+ # Create json file for PxGrid Connect as another method pushing context-in
+        with open('data.json', 'w') as file:
+            asset_list_string=json.dumps(asset_list)
+            pxgrid_direct = '{"result":' + asset_list_string + '}'
+            file.write(pxgrid_direct)
+        file.close    
+       
         return components_with_ip_and_label
 
     except requests.exceptions.HTTPError as e:
