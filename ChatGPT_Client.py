1f3b5cf7b47c8c04d3237ae469e834bf8c1d4a51
Nir <nir-r@bynet.co.il>
2023-06-02 10:58:04 +0300

    !!!

--- ChatGPT_Client.py
+++ ChatGPT_Client.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-openai.api_key = os.getenv("OPENAI_KEY")
+openai.api_key = os.getenv("OPEN_AI_KEY")
 
 question = input('What is your question: ')
 
